c8936f374105fc0f444a5ce424ffb9edfb0074a4
Jacinto Aisa Ibañez <jacinto_aisa@hotmail.com>
2024-04-09 12:37:35 +0200

    DSDSDSD

--- EjercicioPersona/wwwroot/js/main.js
+++ EjercicioPersona/wwwroot/js/main.js
@@ -1,24 +1,166 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-window.alert("hola");
-var Configuradores_js_1 = require("./Configuradores.js");
-var ConfiguradorGeneral = new Configuradores_js_1.ConfiguradorEspañolBasico();
-var GeneradorHTML = ConfiguradorGeneral.dameGenerador();
-var _formulario = document.getElementById("formulario");
+class MiPrimerHTML {
+    g;
+    constructor(LibreriaHTML) {
+        this.g = LibreriaHTML;
+    }
+    dameHTML() {
+        let contenido = this.g.dameCss();
+        contenido += this.g.dameDiv("formulario");
+        contenido += "<br />";
+        contenido += this.g.dameTextBox("nombre1", "nombre");
+        contenido += this.g.dameTextBox("nombreIntermedio", "intermedio");
+        contenido += this.g.dameTextBox("apellido1", "apellido1");
+        contenido += this.g.dameTextBox("apellido2", "apellido2");
+        contenido += "<br />";
+        contenido += this.g.dameTextBox("identificativo", "identificativo");
+        contenido += this.g.dameNumberBox("nac", "año nacimiento");
+        contenido += this.g.dameCheckBox("activo", "Activo");
+        contenido += this.g.dameBoton("boton", "Dale");
+        return contenido;
+    }
+}
+class ConfiguradorEspañolBasico {
+    dameGenerador() {
+        return new MiPrimerHTML(new HTMLBootStrap());
+    }
+    dameCreador() {
+        return new CreadorManualEspañol();
+    }
+    dameValidador() {
+        return new ValidadorEspañol();
+    }
+    dameMostrador() {
+        return new MuestraHTML();
+    }
+}
+class CreadorManualEspañol {
+    damePersona() {
+        let MiPersona = new Persona();
+        MiPersona.primerNombre = "Jacinto";
+        MiPersona.nombreIntermedio = "";
+        MiPersona.apellido1 = "Aisa";
+        MiPersona.apellido2 = "Ibañez";
+        MiPersona.añoNacimiento = 2000;
+        MiPersona.identificador = "2323232";
+        MiPersona.activo = true;
+        return MiPersona;
+    }
+}
+class CreadorHTML {
+    damePersona() {
+        let MiPersona = new Persona();
+        MiPersona.primerNombre = this.dameValorTexto("nombre1");
+        MiPersona.nombreIntermedio = this.dameValorTexto("nombreIntermedio");
+        MiPersona.apellido1 = this.dameValorTexto("apellido1");
+        MiPersona.apellido2 = this.dameValorTexto("apellido2");
+        MiPersona.identificador = this.dameValorTexto("identificativo");
+        MiPersona.añoNacimiento = this.dameValorNumero("nac");
+        MiPersona.activo = this.dameValorBooleano("activo");
+        return MiPersona;
+    }
+    dameValorNumero(elementoId) {
+        return Number(this.dameValorTexto(elementoId));
+    }
+    dameValorBooleano(elementoId) {
+        return document.getElementById(elementoId).checked;
+    }
+    dameValorTexto(elementoId) {
+        return document.getElementById(elementoId).value;
+    }
+}
+class HTMLChurrutero {
+    dameDiv(id) {
+        return (`<div id='${id}'></div><br/>`);
+    }
+    dameCss() {
+        return "<br />";
+    }
+    dameBoton(id, texto) {
+        return `<input type='button' id=${id} value='${texto}' />`;
+    }
+    dameCheckBox(id, contenido) {
+        return `<input type='checkbox' id='${id}'/><label for='${id}'> ${contenido} </label>`;
+    }
+    dameTextBox(id, placeholder) {
+        return `<input type ='text' id='${id}' placeholder='${placeholder}'/>`;
+    }
+    dameNumberBox(id, placeholder) {
+        return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
+    }
+}
+class HTMLBootStrap {
+    dameDiv(id) {
+        return (`<div id='${id}' class='form-group col-md-6'></div><br/>`);
+    }
+    dameCss() {
+        return `<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'>`;
+    }
+    dameBoton(id, texto) {
+        return `<input type='button' id=${id} value='${texto}'/>`;
+    }
+    dameCheckBox(id, contenido) {
+        return `<input type='checkbox' id='${id}'/><label for='${id}'> ${contenido} </label>`;
+    }
+    dameTextBox(id, placeholder) {
+        return `<input type ='text' id='${id}' placeholder='${placeholder}' class='form-control'>`;
+    }
+    dameNumberBox(id, placeholder) {
+        return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
+    }
+}
+class MuestraHTML {
+    dameContenido(MiPersona) {
+        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido1} </p>`);
+    }
+}
+class MuestraHTML2 {
+    dameContenido(MiPersona) {
+        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido2} </p>`);
+    }
+}
+class Persona {
+    primerNombre = "";
+    nombreIntermedio = "";
+    apellido1 = "";
+    apellido2 = "";
+    añoNacimiento = 0;
+    identificador = "";
+    activo = true;
+}
+class ValidadorEspañol {
+    isValid(MiPersona) {
+        return (MiPersona.identificador.length > 0 &&
+            MiPersona.apellido1.length > 0 &&
+            MiPersona.apellido2.length > 0 &&
+            MiPersona.primerNombre.length > 0);
+    }
+}
+class ValidadorIngles {
+    isValid(MiPersona) {
+        return (MiPersona.identificador.length > 0 &&
+            MiPersona.apellido1.length > 0 &&
+            MiPersona.nombreIntermedio.length > 0 &&
+            MiPersona.primerNombre.length > 0);
+    }
+}
+let ConfiguradorGeneral = new ConfiguradorEspañolBasico();
+let GeneradorHTML = ConfiguradorGeneral.dameGenerador();
+let _formulario = document.getElementById("formulario");
 if (_formulario != null) {
     _formulario.innerHTML = GeneradorHTML.dameHTML().toString();
 }
-var _boton = document.getElementById("boton");
+let _boton = document.getElementById("boton");
 if (_boton != null) {
     _boton.addEventListener("click", valida);
 }
 function valida() {
-    var Creador = ConfiguradorGeneral.dameCreador();
-    var ValidadorPersona = ConfiguradorGeneral.dameValidador();
-    var Mostrador = ConfiguradorGeneral.dameMostrador();
-    var MiPersona = Creador.damePersona();
-    var _verde = document.getElementById("verde");
-    var _rojo = document.getElementById("rojo");
+    let Creador = ConfiguradorGeneral.dameCreador();
+    let ValidadorPersona = ConfiguradorGeneral.dameValidador();
+    let Mostrador = ConfiguradorGeneral.dameMostrador();
+    let MiPersona = Creador.damePersona();
+    let _verde = document.getElementById("verde");
+    let _rojo = document.getElementById("rojo");
     if (ValidadorPersona.isValid(MiPersona)) {
         if (_verde != null) {
             _verde.innerHTML = Mostrador.dameContenido(MiPersona).toString();
--- EjercicioPersona/wwwroot/js/main.ts
+++ EjercicioPersona/wwwroot/js/main.ts
@@ -1,10 +1,183 @@
-﻿window.alert("hola");
-import { IValidable } from "./Validadores.js";
-import { IMuestra } from "./Mostradores.js";
-import { IConfigurable,ConfiguradorEspañolBasico } from "./Configuradores.js";
-import { IPersonable } from "./Creadores.js";
-import { IGeneraHTML } from "./GeneradoresHTML.js";
+﻿interface IGeneraHTML {
+    dameHTML(): String;
+}
+class MiPrimerHTML implements IGeneraHTML {
+    g: ILibreriaHTML;
+    constructor(LibreriaHTML: ILibreriaHTML) {
+        this.g = LibreriaHTML;
+    }
+    dameHTML(): String {
+        let contenido = this.g.dameCss();
+        contenido += this.g.dameDiv("formulario");
+        contenido += "<br />";
+        contenido += this.g.dameTextBox("nombre1", "nombre");
+        contenido += this.g.dameTextBox("nombreIntermedio", "intermedio");
+        contenido += this.g.dameTextBox("apellido1", "apellido1");
+        contenido += this.g.dameTextBox("apellido2", "apellido2");
+        contenido += "<br />";
+        contenido += this.g.dameTextBox("identificativo", "identificativo");
+        contenido += this.g.dameNumberBox("nac", "año nacimiento");
+        contenido += this.g.dameCheckBox("activo", "Activo");
+        contenido += this.g.dameBoton("boton", "Dale");
+        return contenido;
+    }
+}
+interface IConfigurable {
+    dameGenerador(): IGeneraHTML;
+    dameCreador(): IPersonable;
+    dameValidador(): IValidable;
+    dameMostrador(): IMuestra;
+}
+class ConfiguradorEspañolBasico implements IConfigurable {
+    dameGenerador(): IGeneraHTML {
+        return new MiPrimerHTML(new HTMLBootStrap());
+    }
+    dameCreador(): IPersonable {
+        return new CreadorHTML();
+    }
+    dameValidador(): IValidable {
+        return new ValidadorIngles();
+    }
+    dameMostrador(): IMuestra {
+        return new MuestraHTML();
+    }
 
+}
+interface IPersonable {
+    damePersona(): Persona;
+}
+
+
+class CreadorManualEspañol implements IPersonable {
+    damePersona(): Persona {
+        let MiPersona = new Persona();
+        MiPersona.primerNombre = "Jacinto";
+        MiPersona.nombreIntermedio = "";
+        MiPersona.apellido1 = "Aisa";
+        MiPersona.apellido2 = "Ibañez";
+        MiPersona.añoNacimiento = 2000;
+        MiPersona.identificador = "2323232";
+        MiPersona.activo = true;
+        return MiPersona;
+    }
+}
+
+class CreadorHTML implements IPersonable {
+    damePersona(): Persona {
+        let MiPersona: Persona = new Persona();
+        MiPersona.primerNombre = this.dameValorTexto("nombre1");
+        MiPersona.nombreIntermedio = this.dameValorTexto("nombreIntermedio");
+        MiPersona.apellido1 = this.dameValorTexto("apellido1");
+        MiPersona.apellido2 = this.dameValorTexto("apellido2");
+        MiPersona.identificador = this.dameValorTexto("identificativo");
+        MiPersona.añoNacimiento = this.dameValorNumero("nac");
+        MiPersona.activo = this.dameValorBooleano("activo");
+        return MiPersona;
+    }
+
+    private dameValorNumero(elementoId: string): number {
+        return Number(this.dameValorTexto(elementoId));
+    }
+    private dameValorBooleano(elementoId: string): boolean {
+        return (<HTMLInputElement>document.getElementById(elementoId)).checked;
+    }
+    private dameValorTexto(elementoId: string) {
+        return (<HTMLInputElement>document.getElementById(elementoId)).value;
+    }
+}
+
+interface ILibreriaHTML {
+    dameCss(): string;
+    dameTextBox(id: string, placeholder: string): string;
+    dameNumberBox(id: string, placeholder: string): string;
+    dameCheckBox(id: string, contenido: string): string;
+    dameBoton(id: string, texto: string): string;
+    dameDiv(id: string): string;
+}
+class HTMLChurrutero implements ILibreriaHTML {
+    public dameDiv(id: string): string {
+        return (`<div id='${id}'></div><br/>`);
+    }
+    public dameCss(): string {
+        return "<br />";
+    }
+    public dameBoton(id: string, texto: string): string {
+        return `<input type='button' id=${id} value='${texto}' />`;
+    }
+    public dameCheckBox(id: string, contenido: string): string {
+        return `<input type='checkbox' id='${id}'/><label for='${id}'> ${contenido} </label>`;
+    }
+    public dameTextBox(id: string, placeholder: string): string {
+        return `<input type ='text' id='${id}' placeholder='${placeholder}'/>`;
+    }
+    public dameNumberBox(id: string, placeholder: string): string {
+        return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
+    }
+}
+class HTMLBootStrap implements ILibreriaHTML {
+    public dameDiv(id: string): string {
+        return (`<div id='${id}' class='form-group col-md-6'></div><br/>`);
+    }
+    public dameCss(): string {
+        return `<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'>`;
+    }
+    public dameBoton(id: string, texto: string): string {
+        return `<input type='button' id=${id} value='${texto}'/>`;
+    }
+    public dameCheckBox(id: string, contenido: string): string {
+        return `<input type='checkbox' id='${id}'/><label for='${id}'> ${contenido} </label>`;
+    }
+    public dameTextBox(id: string, placeholder: string): string {
+        return `<input type ='text' id='${id}' placeholder='${placeholder}' class='form-control'>`;
+    }
+    public dameNumberBox(id: string, placeholder: string): string {
+        return `<input type='number' id='${id}' placeholder='${placeholder}' />`;
+    }
+
+}
+interface IMuestra {
+    dameContenido(MiPersona: Persona): String;
+}
+class MuestraHTML implements IMuestra {
+    dameContenido(MiPersona: Persona): String {
+        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido1} </p>`);
+    }
+}
+class MuestraHTML2 implements IMuestra {
+    dameContenido(MiPersona: Persona): String {
+        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido2} </p>`);
+    }
+}
+class Persona {
+    primerNombre: string = "";
+    nombreIntermedio: string = "";
+    apellido1: string = "";
+    apellido2: string = "";
+    añoNacimiento: number = 0;
+    identificador: string = "";
+    activo: boolean = true;
+}
+interface IValidable {
+    isValid(MiPersona: Persona): boolean;
+}
+
+class ValidadorEspañol implements IValidable {
+    isValid(MiPersona: Persona): boolean {
+        return (MiPersona.identificador.length > 0 &&
+            MiPersona.apellido1.length > 0 &&
+            MiPersona.apellido2.length > 0 &&
+            MiPersona.primerNombre.length > 0);
+    }
+}
+
+class ValidadorIngles implements IValidable {
+    isValid(MiPersona: Persona): boolean {
+        return (MiPersona.identificador.length > 0 &&
+            MiPersona.apellido1.length > 0 &&
+            MiPersona.nombreIntermedio.length > 0 &&
+            MiPersona.primerNombre.length > 0);
+    }
+}
 let ConfiguradorGeneral: IConfigurable = new ConfiguradorEspañolBasico();
 let GeneradorHTML: IGeneraHTML = ConfiguradorGeneral.dameGenerador();
 let _formulario = document.getElementById("formulario");
